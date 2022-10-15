992359af8c1ea666af8d0ccade0180dd3e785f27
anurag kachhala <kachhalaanurag@gmail.com>
2022-10-15 08:25:38 +0530

    initial package set up

--- app/src/main/java/com/hackathon/rickyandmorty/prasentation/ui/MainActivity.kt
+++ app/src/main/java/com/hackathon/rickyandmorty/prasentation/ui/MainActivity.kt
@@ -1,7 +1,9 @@
-package com.hackathon.rickyandmorty
+package com.hackathon.rickyandmorty.prasentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hackathon.rickyandmorty.R
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
